aef1efc30cfa3ee82e84aa180a5aa10faf5d5d51
pulkit jain <pulkitjaincool28@gmail.com>
2023-07-21 17:50:13 +0530

    add files

---
+++ CosmosGettingStartedTutorial/Model/employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosmosGettingStartedTutorial.Model
+{
+   /// <summary>
+   /// this model is for creating employee
+   /// </summary>
+    public class employee
+    {
+        public string EmployeeName { get; set; }
+        public int Salary { get; set; }
+        public Guid id { get; set; }
+        public int CompanyNumber { get; set; }
+    }
+}
